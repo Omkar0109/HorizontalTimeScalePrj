96d4dc91164ca13067cabf559a7987a04509150c
Omkar0109 <sobp9999@gmail.com>
2024-04-17 00:35:33 +0530

    Develop User Interface

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -1,32 +1,15 @@
-//import { useState } from 'react'
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import './App.css';
+import TimeSpan from './Timespan';
+
+import Data from './Data';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/api/data');
-      setData(response.data);
-      console.log(response.data)
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>JSON Data</h1>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{JSON.stringify(item)}</li>
-        ))}
-      </ul>
+    <div>
+      <TimeSpan/>
+      <br />
+      <Data />
     </div>
   );
 }
